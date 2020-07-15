41ac035ce9a61c3e0c8fb6b88c767b183e3e19d8
Schrese <schreseholloway@gmail.com>
2020-07-15 16:29:31 -0400

    changed for loops into list comprehensions

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,5 @@
+import random
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -45,8 +47,23 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        # for i in range(num_users):
+        #     self.add_user(f"User {i}")
+        [self.add_user(f"User {i}") for i in range(num_users)]
 
         # Create friendships
+        possible_friendships = []
+        # for user_id in self.users: # O(n^2) loop. Can I make this better?
+        #     for friend_id in range(user_id + 1, self.last_id + 1):
+        #         possible_friendships.append((user_id, friend_id))
+        [[possible_friendships.append((user_id, friend_id)) for friend_id in range(user_id + 1, self.last_id + 1)] for user_id in self.users]
+
+        # Shuffle the users
+        random.shuffle(possible_friendships)
+
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
